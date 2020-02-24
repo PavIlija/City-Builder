cf6bc47250337d569944a572b8cadae3a57e3ae8
PavIlija <32486320+PavIlija@users.noreply.github.com>
2020-02-24 20:55:34 +0100

    r

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        Camera.main.orthographicSize=Mathf.Clamp(Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") 
+            * (10f * Camera.main.orthographicSize*.1f),2.5f,50f);    
+    }
+}
